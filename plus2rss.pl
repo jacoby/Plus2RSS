209f86a0c6ffa3614b70b41985f351e26da23ee2
Dave Jacoby <jacoby.david@gmail.com>
2012-02-23 14:16:16 -0500

    added maxResults and file flags

--- plus2rss.pl
+++ plus2rss.pl
@@ -14,7 +14,7 @@ use XML::RSS ;
 use YAML qw{ LoadFile DumpFile } ;
 use subs qw{ webGet handle_plus } ;
 
-my $debug = 0 ;
+my $debug  = 0 ;
 my $config = config() ;
 handle_plus( $config ) ;
 exit ;
@@ -24,65 +24,74 @@ exit ;
 # --------- --------- --------- --------- --------- --------- ---------
 sub handle_plus {
     my ( $config ) = shift ;
-    my $api_base_url =  'https://www.googleapis.com/plus/v1/' ;
-    my $dispatch = {
-        get_activities => 'people/USER_ID/activities/public' ,
-        } ;
-    my $api_url = $api_base_url .
-        $dispatch->{ get_activities } .
-        '?key=' . $config->{ google_api_key } .
-        '&alt=json'
-        ;
+    my $api_base_url = 'https://www.googleapis.com/plus/v1/' ;
+    my $dispatch = { get_activities => 'people/USER_ID/activities/public', } ;
+    my $maxResults = '&maxResults=' . $config->{ maxResults } ;
+    my $api_url =
+          $api_base_url
+        . $dispatch->{ get_activities } . '?key='
+        . $config->{ google_api_key }
+        . $maxResults
+        . '&alt=json' ;
     my $user_id = $config->{ google_user_id } ;
     $api_url =~ s/USER_ID/$user_id/ ;
 
-    my $data = webGet $api_url ;
-    my $json = decode_json $data ;
-    my $output_feed = XML::RSS->new( version=> '2.0' ) ;
+    my $data        = webGet $api_url ;
+    my $json        = decode_json $data ;
+    my $output_feed = XML::RSS->new( version => '2.0' ) ;
     $output_feed->channel(
-        title => $json->{ title } ,
-        link  => $json->{ selfLink } ,
-        description  => 'From my made-myself Plus2RSS Feed',
+        title       => $json->{ title },
+        link        => $json->{ selfLink },
+        description => 'From my made-myself Plus2RSS Feed',
         ) ;
     my $items = $json->{ items } ;
+
     for my $item ( @$items ) {
-        my $title = $item->{ title } ;
-        my $desc = $item->{ object }->{ content } ;
-        my $link = $item->{ url } ;
+        my $title   = $item->{ title } ;
+        my $desc    = $item->{ object }->{ content } ;
+        my $link    = $item->{ url } ;
         my $pubDate = $item->{ published } ;
         $output_feed->add_item(
-            title => $title ,
-            link => $link ,
-            description => $desc ,
-            pubDate => $pubDate ,
+            title       => $title,
+            link        => $link,
+            description => $desc,
+            pubDate     => $pubDate,
             ) ;
         }
-    say $output_feed->as_string ;
+    if ( $config->{ output_file } ) {
+        $output_feed->save( $config->{ output_file } ) ;
+        }
+    else {
+        say $output_feed->as_string ;
+        }
     }
+
 # --------- --------- --------- --------- --------- --------- ---------
 
 # ========= ========= ========= ========= ========= ========= =========
 # give it a URL, get back the download
 # --------- --------- --------- --------- --------- --------- ---------
 sub webGet {
-  my $url = shift ;
-  my $agent         = new LWP::UserAgent ;
-     #$agent->timeout(60) ;
-  my $request       = new HTTP::Request('GET',$url) ;
-  my $response      = $agent->request($request) ;
-  my $status = $response->as_string ;
-  $debug and print qq(<getting url="$url">\n) ;
-  if ( $response->is_success ) {
-    my $content =  $response->content ;
-    $debug and print qq(<get result="success">\n) ;
-    $debug and print qq(<get result="$status">\n) ;
-    return $content ;
+    my $url   = shift ;
+    my $agent = new LWP::UserAgent ;
+
+    #$agent->timeout(60) ;
+    my $request  = new HTTP::Request( 'GET', $url ) ;
+    my $response = $agent->request( $request ) ;
+    my $status   = $response->as_string ;
+    $debug and print qq(<getting url="$url">\n) ;
+    if ( $response->is_success ) {
+        my $content = $response->content ;
+        $debug and print qq(<get result="success">\n) ;
+        $debug and print qq(<get result="$status">\n) ;
+        return $content ;
+        }
+    else {
+        $debug and print qq(<get result="$status">\n) ;
+        return undef ;
+        }
     }
-  else {
-    $debug and print qq(<get result="$status">\n) ;
-    return undef ;
-    }
-  }
+
 # --------- --------- --------- --------- --------- --------- ---------
 
 # ========= ========= ========= ========= ========= ========= =========
@@ -94,6 +103,11 @@ sub config {
     if ( $config_file && -f $config_file ) {
         $config = LoadFile( $config_file ) ;
         }
+    GetOptions(
+        'file=s'       => \$config->{ output_file },
+        'maxResults=i' => \$config->{ maxResults },
+        ) ;
     return $config ;
     }
+
 # --------- --------- --------- --------- --------- --------- ---------
